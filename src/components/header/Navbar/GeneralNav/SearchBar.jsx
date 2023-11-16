4776a9a2d89777e54faa40d895aef83902156426
Vinnydaninja <vincenguyen35@gmail.com>
2023-11-15 18:52:22 -0800

    add rotating placeholder for search bar and minor layout fixes

--- src/components/header/Navbar/GeneralNav/SearchBar.jsx
+++ src/components/header/Navbar/GeneralNav/SearchBar.jsx
@@ -1,11 +1,46 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+// react imports
+import { useState, useEffect } from "react";
+
+// component imports
 import searchIcon from "../../../../assets/logos/searchIcon.png";
 
 const SearchBar = () => {
+    const placeholderItemsList = [
+        "Dress Cocktail",
+        "Cargos",
+        "Cargo Pants",
+        "Men Navy Blue",
+        "Oversized Tshirt",
+        "Brides Maid Dress",
+        "Jeans",
+        "Cardigan",
+        "Long Sleeve Men",
+        "Wedding Guest Dress"
+    ];
+
+    const [placeholderItem, setPlaceholderItem] = useState(placeholderItemsList[0]);
+
+    useEffect(() => {
+        let i = 0;
+        const rotatePlaceholderItem = () => {
+            i++;
+            setPlaceholderItem(placeholderItemsList[i]);
+            if(i === 9) {
+                i = -1;
+            }
+        }
+        setInterval(rotatePlaceholderItem, 4000);
+    }, []);
+
     return (
         <form action="">
-            <input type="text" />
+            <input
+                type="text"
+                placeholder={ placeholderItem }
+            />
             <img
-                src={searchIcon}
+                src={ searchIcon }
                 alt="Search Icon"
             />
         </form>
