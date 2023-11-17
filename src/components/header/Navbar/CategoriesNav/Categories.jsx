e6c6221443ff484bb9551d8028f2ee9e3c698f6d
Vinnydaninja <vincenguyen35@gmail.com>
2023-11-16 19:32:09 -0800

    add arrow click and hover events

--- src/components/header/Navbar/CategoriesNav/Categories.jsx
+++ src/components/header/Navbar/CategoriesNav/Categories.jsx
@@ -1,10 +1,21 @@
+// react imports
+import { useState } from "react";
+
+// component imports
 import downArrowGreyIcon from "../../../../assets/logos/downArrowGreyIcon.png";
 
 const Categories = () => {
+    const [hover, setHover] = useState("");
+
     return (
-        <div className="dropdown">
+        <div
+            className="dropdown"
+            onMouseOver={ () => setHover("hovering") }
+            onMouseLeave={ () => setHover("notHovering") }
+        >
             Categories
             <img
+                className={ hover }
                 src={ downArrowGreyIcon }
                 alt="directional icon for categories"
             />
--- src/components/header/Navbar/CategoriesNav/CategoriesScroller.jsx
+++ src/components/header/Navbar/CategoriesNav/CategoriesScroller.jsx
@@ -1,17 +1,42 @@
+// react imports
+import { useState } from "react";
+
+// img imports
 import leftArrowBlackIcon from "../../../../assets/logos/leftArrowBlackIcon.png";
 import leftArrowGreyIcon from "../../../../assets/logos/leftArrowGreyIcon.png";
 import rightArrowBlackIcon from "../../../../assets/logos/rightArrowBlackIcon.png";
 import rightArrowGreyIcon from "../../../../assets/logos/rightArrowGreyIcon.png";
 
 const CategoriesScroller = () => {
+    const [leftArrow, setLeftArrow] = useState(leftArrowGreyIcon);
+    const [rightArrow, setRightArrow] = useState(rightArrowBlackIcon);
+
+    const handleLeftArrow = () => {
+        if(leftArrow === leftArrowGreyIcon) {
+            setLeftArrow(leftArrowBlackIcon);
+        } else {
+            setLeftArrow(leftArrowGreyIcon);
+        }
+    }
+
+    const handleRightArrow = () => {
+        if(rightArrow === rightArrowGreyIcon) {
+            setRightArrow(rightArrowBlackIcon);
+        } else {
+            setRightArrow(rightArrowGreyIcon);
+        }
+    }
+
     return (
         <div className="categories-scroller">
             <img
-                src={ leftArrowGreyIcon }
+                onClick={ handleLeftArrow }
+                src={ leftArrow }
                 alt="right arrow icon"
             />
             <img
-                src={ rightArrowBlackIcon }
+                onClick={ handleRightArrow }
+                src={ rightArrow }
                 alt="right arrow icon"
             />
         </div>
