9339fb93c089c07bc6307259c7086ec77e510107
Vinnydaninja <vincenguyen35@gmail.com>
2023-11-28 17:53:06 -0800

    Add hover effects for miscellaneious nav

---
+++ src/components/header/Navbar/GeneralNav/Dropdown.jsx
@@ -0,0 +1,9 @@
+const Dropdown = () => {
+    return (
+        <div className="dropdown">
+            dropdown Here
+        </div>
+    );
+}
+ 
+export default Dropdown;
--- src/components/header/Navbar/GeneralNav/MiscellaneousNav.jsx
+++ src/components/header/Navbar/GeneralNav/MiscellaneousNav.jsx
@@ -1,3 +1,7 @@
+// React Imports
+import { useState } from "react";
+
+// Component Imports
 import userIcon from "../../../../assets/logos/userIcon.png";
 import shoppingCartIcon from "../../../../assets/logos/shoppingCartIcon.png";
 import heartIconGrey from "../../../../assets/logos/heartIconGrey.png";
@@ -5,28 +9,75 @@ import headphonesIcon from "../../../../assets/logos/headphonesIcon.png";
 import globeIcon from "../../../../assets/logos/globeIcon.png";
 
 const MiscellaneousNav = () => {
+    const [shoppingCartItemsTotal, setShoppingCartItemsTotal] = useState(0);
+    const [favoriteItemsTotal, setFavoriteTotal] = useState(0);
+    const [userHover, setUserHover] = useState(false);
+    const [shoppingCartHover, setShoppingCartHover] = useState(false);
+    const [supportHover, setSupportHover] = useState(false);
+    const [internationalHover, setInternationalHover] = useState(false);
+
     return (
         <div className="general-nav-icons">
-            <img
-                src={userIcon}
-                alt="User Icon"
-            />
-            <img
-                src={shoppingCartIcon}
-                alt="Shopping Cart Icon"
-            />
-            <img
-                src={heartIconGrey}
-                alt="Heart Icon"
-            />
-            <img
-                src={headphonesIcon}
-                alt="Headphones Icon"
-            />
-            <img
-                src={globeIcon}
-                alt="Globe Icon"
-            />
+            <div
+                className="user-settings"
+                onMouseOver={() => setUserHover(true)}
+                onMouseOut={() => setUserHover(false)}
+            >
+                <img
+                    src={userIcon}
+                    alt="User Icon"
+                />
+            </div>
+            <div
+                className="shopping-cart"
+                onMouseOver={() => setShoppingCartHover(true)}
+                onMouseOut={() => setShoppingCartHover(false)}
+            >
+                <img
+                    src={shoppingCartIcon}
+                    alt="Shopping Cart Icon"
+                />
+                { shoppingCartItemsTotal }
+            </div>
+            <div className="favorites">
+                <img
+                    src={heartIconGrey}
+                    alt="Heart Icon"
+                />
+                { favoriteItemsTotal }
+            </div>
+            <div
+                className="support"
+                onMouseOver={() => setSupportHover(true)}
+                onMouseOut={() => setSupportHover(false)}
+            >
+                <img
+                    src={headphonesIcon}
+                    alt="Headphones Icon"
+                />
+            </div>
+            <div
+                className="international"
+                onMouseOver={() => setInternationalHover(true)}
+                onMouseOut={() => setInternationalHover(false)}
+            >
+                <img
+                    src={globeIcon}
+                    alt="Globe Icon"
+                />
+            </div>
+            {
+                userHover && <div className="dropdown">User Hover</div>
+            }
+            {
+                shoppingCartHover && <div className="dropdown">ShoppingCartHover</div>
+            }
+            {
+                supportHover && <div className="dropdown">Support Hover</div>
+            }
+            {
+                internationalHover && <div className="dropdown">International Hover</div>
+            }
         </div>
     );
 }
