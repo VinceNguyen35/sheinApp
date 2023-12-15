c5cfffa1b53dbe0853e31f77f4b62863c40ec5bc
Vinnydaninja <vincenguyen35@gmail.com>
2023-12-14 16:31:20 -0800

    Finished styling product accordion's Size and Fit

--- src/components/productIntro/productAccordion/SizeAndFit.jsx
+++ src/components/productIntro/productAccordion/SizeAndFit.jsx
@@ -11,6 +11,18 @@ const SizeAndFit = () => {
     const sizeAndFitData = product.sizeAndFitData;
 
     const [isDisplayed, setIsDisplayed] = useState(false);
+    const [cmSelectionClass, setCMSelectionClass] = useState("measurement-type");
+    const [inchSelectionClass, setInchSelectionClass] = useState("measurement-type selected");
+
+    const handleMeasurementSelection = (measurement) => {
+        if(measurement === "CM") {
+            setCMSelectionClass("measurement-type selected");
+            setInchSelectionClass("measurement-type");
+        } else {
+            setCMSelectionClass("measurement-type");
+            setInchSelectionClass("measurement-type selected");
+        }
+    }
 
     return (
         <div className="accordion">
@@ -24,42 +36,57 @@ const SizeAndFit = () => {
             {
                 isDisplayed &&
                 <div className="accordion-details">
-                    <h6>Product Measurements</h6>
-                    <div><span> CM </span><span> IN </span></div>
-                    <table>
-                        <thead>
-                            <tr>
+                    <div className="size-and-fit">
+                        <div className="size-and-fit-header">Product Measurements</div>
+                        <div className="measurement-types">
+                            <span
+                                className={ cmSelectionClass }
+                                onClick={ () => { handleMeasurementSelection("CM") } }
+                            >
+                                CM
+                            </span>
+                            <span
+                                className={ inchSelectionClass }
+                                onClick={ () => { handleMeasurementSelection("IN") } }
+                            >
+                                IN
+                            </span>
+                        </div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    {
+                                        sizeAndFitHeaders.map((header, index) => (
+                                            <th
+                                                key={index}
+                                            >
+                                                {header}
+                                            </th>
+                                        ))
+                                    }
+                                </tr>
+                            </thead>
+                            <tbody>
                                 {
-                                    sizeAndFitHeaders.map((header, index) => (
-                                        <th
+                                    sizeAndFitData.map((row, index) => (
+                                        <tr
                                             key={index}
                                         >
-                                            {header}
-                                        </th>
+                                            {
+                                                row.map((data, index) => (
+                                                    <td
+                                                        key={index}
+                                                    >
+                                                        {data}
+                                                    </td>
+                                                ))
+                                            }
+                                        </tr>
                                     ))
                                 }
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                sizeAndFitData.map((row, index) => (
-                                    <tr
-                                        key={index}
-                                    >
-                                        {
-                                            row.map((data, index) => (
-                                                <td
-                                                    key={index}
-                                                >
-                                                    {data}
-                                                </td>
-                                            ))
-                                        }
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             }
         </div>
