b4c0973788f5b3943216fea632f06ec0d7ff32ad
Vinnydaninja <vincenguyen35@gmail.com>
2023-12-28 16:39:20 -0800

    Add footer form error messages and handling

--- src/components/footer/companyInfo/contactInfo/ContactInfo.jsx
+++ src/components/footer/companyInfo/contactInfo/ContactInfo.jsx
@@ -1,3 +1,6 @@
+// React Imports
+import { useState } from "react";
+
 // Component Imports
 import SocialMedia from "./SocialMedia";
 import AppDownload from "./AppDownload";
@@ -6,6 +9,34 @@ import AppDownload from "./AppDownload";
 import downArrowBlackIcon from "../../../../assets/logos/downArrowBlackIcon.png";
 
 const ContactInfo = () => {
+
+    const [email, setEmail] = useState("");
+    const [emailErrorMessage, setEmailErrorMessage] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [phoneNumberErrorMessage, setPhoneNumberErrorMessage] = useState("");
+
+    const handleEmailSubscribe = (event) => {
+        event.preventDefault();
+        if (email.includes("@") && email.includes(".")) { // input validation
+            setEmailErrorMessage("");
+        } else if (email === "") { // check for empty string
+            setEmailErrorMessage("Please enter an email address.");
+        } else {
+            setEmailErrorMessage("Oops! It looks like you entered an invalid email address.");
+        }
+    }
+
+    const handlePhoneSubscribe = (event) => {
+        event.preventDefault();
+        if (phoneNumber.length === 10 && !isNaN(phoneNumber)) {
+            setPhoneNumberErrorMessage("");
+        } else if (phoneNumber === "") {
+            setPhoneNumberErrorMessage("Please enter your WhatsApp account.");
+        } else {
+            setPhoneNumberErrorMessage("Oops! It looks like you entered an invalid WhatsApp account.");
+        }
+    }
+
     return (
         <div className="contact-info">
             <div className="row">
@@ -20,32 +51,70 @@ const ContactInfo = () => {
             </div>
             <div className=" row signup-forms">
                 <h6>SIGN UP FOR SHEIN STYLE NEWS</h6>
-                <form action="#" method="POST">
-                    <input
-                        type="text"
-                        placeholder="Your Email Address"
-                    />
-                    <div className="subscribe">Subscribe</div>
-                </form>
-                <form action="#" method="POST">
-                    <div className="phone-country-code">
-                        <span>US + 1</span>
-                        <img src={ downArrowBlackIcon } alt="down arrow" />
+                <form
+                    action="#"
+                    method="POST"
+                    onSubmit={ event => handleEmailSubscribe(event) }
+                >
+                    <div className="footer-input">
+                        <input
+                            type="text"
+                            placeholder="Your Email Address"
+                            value={ email }
+                            onChange={ event =>  setEmail(event.target.value) }
+                        />
+                        <div className="footer-tips">
+                            <div className="input-error-text">
+                                { emailErrorMessage }
+                            </div>
+                        </div>
                     </div>
-                    <input
-                        type="text"
-                        placeholder="WhatsApp Account"
-                    />
-                    <div className="subscribe">Subscribe</div>
+                    <div
+                        className="subscribe"
+                        onClick={ event => handleEmailSubscribe(event) }
+                    >
+                        Subscribe
+                    </div>
                 </form>
-            </div>
-            <div className="privacy-statement">
-                <p>
-                    By clicking the SUBSCRIBE button,
-                    you are agreeing to our <span><a href="https://us.shein.com/Privacy-Security-Policy-a-282.html">Privacy & Cookie Policy</a></span>. 
-                    If you want to unsubsribe the marketing email, 
-                    please proceed to our <span><a href="https://us.shein.com/user/privacy_request?isVisitor=true">privacy center</a></span>.
-                </p>
+                <form
+                    action="#"
+                    method="POST"
+                    onSubmit={ event => handlePhoneSubscribe(event) }
+                >
+                    <div className="footer-input">
+                        <div className="phone">
+                            <div className="phone-country-code">
+                                <span>US + 1</span>
+                                <img src={ downArrowBlackIcon } alt="down arrow" />
+                            </div>
+                            <input
+                                type="text"
+                                placeholder="WhatsApp Account"
+                                value={ phoneNumber }
+                                onChange={ event => setPhoneNumber(event.target.value) }
+                            />
+                        </div>
+                        <div className="footer-tips">
+                            <div className="input-error-text">
+                                { phoneNumberErrorMessage }
+                            </div>
+                        </div>
+                        <div className="privacy-statement">
+                            <p>
+                                By clicking the SUBSCRIBE button,
+                                you are agreeing to our <span><a href="https://us.shein.com/Privacy-Security-Policy-a-282.html">Privacy & Cookie Policy</a></span>.
+                                If you want to unsubsribe the marketing email,
+                                please proceed to our <span><a href="https://us.shein.com/user/privacy_request?isVisitor=true">privacy center</a></span>.
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        className="subscribe"
+                        onClick={ event => handlePhoneSubscribe(event) }
+                    >
+                        Subscribe
+                    </div>
+                </form>
             </div>
         </div>
     );
