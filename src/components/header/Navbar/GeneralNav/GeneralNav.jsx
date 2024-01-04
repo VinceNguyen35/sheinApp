2c801466f274f9e0ff03d92d74a572b60a186068
Vinnydaninja <vincenguyen35@gmail.com>
2024-01-03 16:58:36 -0800

    Made navbar mobile friendly using mobile first design

--- src/components/header/Navbar/GeneralNav/GeneralNav.jsx
+++ src/components/header/Navbar/GeneralNav/GeneralNav.jsx
@@ -1,25 +1,70 @@
+// React Imports
+import { useState, useEffect } from "react";
+
 // Component Imports
-import sheinLogo from "../../../../assets/logos/sheinLogo.png";
 import SearchBar from "./SearchBar";
 import MiscellaneousNav from "./MiscellaneousNav";
 
+// Img Imports
+import sheinLogo from "../../../../assets/logos/sheinLogo.png";
+import leftArrowBlackIcon from "../../../../assets/logos/leftArrowBlackIcon.png";
+import navbarIcon from "../../../../assets/logos/navbarIcon.png";
+
 const GeneralNav = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 480);
+
+    const updateMedia = () => {
+        setIsMobile(window.innerWidth < 480);
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", updateMedia);
+        return () => window.removeEventListener("resize", updateMedia);
+    });
 
     return (
         <div className="general-nav">
             <div className="header-left">
-                <a href="https://us.shein.com">
-                    <img
-                        src={sheinLogo}
-                        alt="Shein Logo"
-                    />
-                </a>
+                {
+                    isMobile &&
+                    <div className="navigation">
+                        <img
+                            src={leftArrowBlackIcon}
+                            alt="left arrow"
+                        />
+                        <img
+                            src={navbarIcon}
+                            alt="Navbar Icon"
+                        />
+                    </div>
+                }
+                {
+                    !isMobile &&
+                    <a href="https://us.shein.com">
+                        <img
+                            src={sheinLogo}
+                            alt="Shein Logo"
+                        />
+                    </a>
+                }
             </div>
             <div className="header-center">
-                <SearchBar />
+                {
+                    isMobile &&
+                    <a href="https://us.shein.com">
+                        <img
+                            src={sheinLogo}
+                            alt="Shein Logo"
+                        />
+                    </a>
+                }
+                {
+                    !isMobile &&
+                    <SearchBar />
+                }
             </div>
             <div className="header-right">
-                <MiscellaneousNav />
+                <MiscellaneousNav isMobile={ isMobile }/>
             </div>
         </div>
     );
--- src/components/header/Navbar/GeneralNav/MiscellaneousNav.jsx
+++ src/components/header/Navbar/GeneralNav/MiscellaneousNav.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 // React Imports
 import { useState } from "react";
 
@@ -7,10 +8,10 @@ import shoppingCartIcon from "../../../../assets/logos/shoppingCartIcon.png";
 import heartIconBlack from "../../../../assets/logos/heartIconBlack.png";
 import headphonesIcon from "../../../../assets/logos/headphonesIcon.png";
 import globeIcon from "../../../../assets/logos/globeIcon.png";
+import searchIconBlack from "../../../../assets/logos/searchIconBlack.png";
+import shareIcon from "../../../../assets/logos/shareIcon.png";
 
-const MiscellaneousNav = () => {
-    const [shoppingCartItemsTotal, setShoppingCartItemsTotal] = useState(0);
-    const [favoriteItemsTotal, setFavoriteTotal] = useState(0);
+const MiscellaneousNav = ({isMobile}) => {
     const [userHover, setUserHover] = useState(false);
     const [shoppingCartHover, setShoppingCartHover] = useState(false);
     const [supportHover, setSupportHover] = useState(false);
@@ -18,54 +19,83 @@ const MiscellaneousNav = () => {
 
     return (
         <div className="miscellaneous-nav">
-            <div
-                className="miscellaneous-icon"
-                onMouseOver={() => setUserHover(true)}
-                onMouseOut={() => setUserHover(false)}
-            >
-                <img
-                    src={userIcon}
-                    alt="User Icon"
-                />
-            </div>
-            <div
-                className="miscellaneous-icon"
-                onMouseOver={() => setShoppingCartHover(true)}
-                onMouseOut={() => setShoppingCartHover(false)}
-            >
-                <img
-                    src={shoppingCartIcon}
-                    alt="Shopping Cart Icon"
-                />
-                { shoppingCartItemsTotal }
-            </div>
-            <div className="miscellaneous-icon">
-                <img
-                    src={heartIconBlack}
-                    alt="Heart Icon"
-                />
-                { favoriteItemsTotal }
-            </div>
-            <div
-                className="miscellaneous-icon"
-                onMouseOver={() => setSupportHover(true)}
-                onMouseOut={() => setSupportHover(false)}
-            >
-                <img
-                    src={headphonesIcon}
-                    alt="Headphones Icon"
-                />
-            </div>
-            <div
-                className="miscellaneous-icon"
-                onMouseOver={() => setInternationalHover(true)}
-                onMouseOut={() => setInternationalHover(false)}
-            >
-                <img
-                    src={globeIcon}
-                    alt="Globe Icon"
-                />
-            </div>
+            {
+                isMobile &&
+                <div className="miscellaneous-nav-mobile">
+                    <div className="miscellaneous-icon">
+                            <img
+                                src={searchIconBlack}
+                                alt="Search Icon"
+                            />
+                    </div>
+                    <div className="miscellaneous-icon">
+                        <img
+                            src={shareIcon}
+                            alt="Share Icon"
+                        />
+                    </div>
+                    <div className="miscellaneous-icon">
+                        <img
+                            src={shoppingCartIcon}
+                            alt="Shopping Cart Icon"
+                        />
+                    </div>
+                </div>
+
+            }
+            {
+                !isMobile &&
+                <div className="miscellaneous-nav-desktop">
+                    <div
+                        className="miscellaneous-icon"
+                        onMouseOver={() => setUserHover(true)}
+                        onMouseOut={() => setUserHover(false)}
+                    >
+                        <img
+                            src={userIcon}
+                            alt="User Icon"
+                        />
+                    </div>
+                    <div
+                        className="miscellaneous-icon"
+                        onMouseOver={() => setShoppingCartHover(true)}
+                        onMouseOut={() => setShoppingCartHover(false)}
+                    >
+                        <img
+                            src={shoppingCartIcon}
+                            alt="Shopping Cart Icon"
+                        />
+                        0
+                    </div>
+                    <div className="miscellaneous-icon">
+                        <img
+                            src={heartIconBlack}
+                            alt="Heart Icon"
+                        />
+                        0
+                    </div>
+                    <div
+                        className="miscellaneous-icon"
+                        onMouseOver={() => setSupportHover(true)}
+                        onMouseOut={() => setSupportHover(false)}
+                    >
+                        <img
+                            src={headphonesIcon}
+                            alt="Headphones Icon"
+                        />
+                    </div>
+                    <div
+                        className="miscellaneous-icon"
+                        onMouseOver={() => setInternationalHover(true)}
+                        onMouseOut={() => setInternationalHover(false)}
+                    >
+                        <img
+                            src={globeIcon}
+                            alt="Globe Icon"
+                        />
+                    </div>
+                </div>
+            }
             {
                 userHover && <div className="dropdown">User Hover</div>
             }
