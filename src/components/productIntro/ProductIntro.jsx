71113a6b59dbfd5302899469d16da9d7308da72e
Vinnydaninja <vincenguyen35@gmail.com>
2024-01-04 16:35:07 -0800

    Add conditional class names based on screen size

--- src/components/productIntro/ProductIntro.jsx
+++ src/components/productIntro/ProductIntro.jsx
@@ -1,4 +1,10 @@
 /* eslint-disable react/prop-types */
+// React Imports
+import { useContext } from "react";
+
+// Context Imports
+import { MobileContext } from "../../context/MobileContext";
+
 // Component Imports
 import ProductImages from "./productImages/ProductImages";
 import ProductInfo from "./ProductInfo";
@@ -8,12 +14,20 @@ import ShippingInfo from "./ShippingInfo";
 import ProductAccordion from "./productAccordion/ProductAccordion";
 
 const ProductIntro = () => {
+    const isMobile = useContext(MobileContext);
+
+    const productIntroClass = isMobile ? "row" : "product-intro";
+
+    const productIntroLeftColClass = isMobile ? "" : "product-intro-col-left";
+
+    const productIntroRightColClass = isMobile ? "" : "product-intro-col-right";
+
     return (
-        <div className="product-intro">
-            <div className="product-intro-col-left">
+        <div className={productIntroClass}>
+            <div className={productIntroLeftColClass}>
                 <ProductImages />
             </div>
-            <div className="product-intro-col-right">
+            <div className={productIntroRightColClass}>
                 <ProductInfo />
                 <SelectOptions />
                 <AddingProduct />
